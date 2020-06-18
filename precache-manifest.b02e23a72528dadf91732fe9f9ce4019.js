680c3d3a7b7443fef40e2d4705529654b5902e04
Tobias Peter <tobias.peter@bluewin.ch>
2020-06-18 20:10:46 +0200

    Updates

---
+++ precache-manifest.b02e23a72528dadf91732fe9f9ce4019.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053becf735d69ea56dc5e6b2e908cf1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92d4890eba9c4ab906d6",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "b7887c9654aef13c87ce",
+    "url": "/static/js/2.19b4e602.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.19b4e602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92d4890eba9c4ab906d6",
+    "url": "/static/js/main.915f1184.chunk.js"
+  },
+  {
+    "revision": "86aa067b7396347ab8cd",
+    "url": "/static/js/runtime-main.f4712e89.js"
+  }
+]);
